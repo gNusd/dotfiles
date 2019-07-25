7629e89a5a85d45b04efa402f1ff55d9385d8d81
gnus <gnusd@telia.com>
2019-07-25 16:12:09 +0200

    2019-07-25

--- dotfile_install.sh
+++ dotfile_install.sh
@@ -1,93 +1,41 @@
 #!/bin/bash
-rd=$(pwd)
 
-# ROOT
-tmp=$HOME/tmp/backup
-if [ ! -e $tmp ]
-then
-		mkdir -p $tmp
-fi
 
-if [ -f $HOME/.bashrc ]
-then
-		mv $HOME/.bashrc $tmp
-fi
-ln -s $rd/.bashrc $HOME/
+# ROOT files
+files=(".bash_aliases" ".bashrc" ".inputrc" ".profile" ".tmux.conf")
 
-if [ -f $HOME/.bash_aliases ]
-then
-		mv $HOME/.bash_aliases $tmp
-fi
-ln -s $rd/.bash_aliases $HOME/
-
-if [ -f $HOME/.inputrc ]
-then
-		mv $HOME/.inputrc $tmp
-fi
-ln -s $rd/.inputrc $HOME/
-
-if [ -f $HOME/.profile ]
-then
-		mv $HOME/.profile $tmp
-fi
-ln -s $rd/.profile $HOME/
-
-if [ -f $HOME/.tmux.conf ]
-then
-		mv $HOME/.tmux.conf $tmp
-fi
-ln -s $rd/.tmux.conf $HOME/
-
+path=$HOME
+for file in "${files[@]}"
+do
+   [ -f $path/$file ] && [ ! -L $path/$file ] && rm $path/$file
+   [ ! -f $path/$file ] && ln -s $(pwd)/$file $path
+done
 
 # .config
 
 conf=$HOME/.config
+[ ! -e $conf ] && mkdir $conf
+dirs=("nvim" "qutebrowser" "triadactyl" "zathura")
+for dir in "${dirs[@]}"
+do
+		[ -e $conf/$dir ] && rm $conf/$dir
+done
 
-if [ ! -e $conf ]
-then
-		mkdir $conf
-fi
+files=("katerc" "katevirc" "plasma-localerc" "plasma-nm" "plasmanotifyrc" "plasma-org.kde.plasma.desktop-appletsrc" "plasmarc" "plasmashellrc")
 
-if [ -f $conf/katerc ]
-then
-		mv $conf/katerc $tmp
-fi
+for file in "${files[@]}"
+do
+		[ -f $conf/$file ] && [ ! -L $conf/$file ] && rm $conf/$file
+done
+ln -s $(pwd)/.config/* $conf
 
-if [ -f $conf/katevirc ]
-then
-		mv $conf/katevirc $tmp
-fi
-
-if [ -e $conf/qutebrowser ]
-then
-		rm -rf $conf/qutebrowser
-fi
-
-if [ -e $conf/tridactyl ]
-then
-		rm -rf $conf/tridactyl
-fi
-
-if [ -e $conf/zathura ]
-then
-		rm -rf $conf/zathura
-fi
-ln -s $rd/.config/* $conf/
 
 # mozilla
-if [ ! -e $HOME/.mozilla ]
-then
-		mkdir -p $HOME/.mozilla/firefox/current/
-fi
-if [ ! -e  $HOME/.mozilla/firefox/current/ ]
-then
-		mkdir -p $HOME/.mozilla/firefox/current/
-fi
-ln -s $rd/.mozilla/firefox/current/* $HOME/.mozilla/firefox/current/
+[ ! -e  $HOME/.mozilla/firefox/current/ ] && mkdir -p $HOME/.mozilla/firefox/current/
+ln -s $(pwd)/.mozilla/firefox/current/* $HOME/.mozilla/firefox/current/
 
 #.ssh
-if [ ! -e $HOME/.ssh ]
-then
-		mkdir $HOME/.ssh
-fi
-ln -s $rd/.ssh/ssh_alias $HOME/.ssh/
+path="$HOME/.ssh"
+[ ! -e $path ] && mkdir $path
+[ -f .$path/ssh_alias ] && [ ! -L $path/ssh_alias ] && rm $path/ssh_alias
+ln -s $(pwd)/.ssh/ssh_alias $path

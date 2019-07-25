89c108a821e255e5831a2b9cc95186ec508c4d18
gnus <gnusd@telia.com>
2019-07-25 12:18:15 +0200

    2019-07-25

--- dotfile_install.sh
+++ dotfile_install.sh
@@ -1,19 +1,89 @@
 #!/bin/bash
+rd=pwd
 
-if [ ! -e $HOME/tmp ]
+# ROOT
+tmp=$HOME/tmp/backup
+if [ ! -e $tmp ]
 then
-		mkdir $HOME/tmp
+		mkdir -p $tmp
 fi
 
 if [ -f $HOME/.bashrc ]
 then
-		mv $HOME/.bashrc $HOME/tmp
+		mv $HOME/.bashrc $tmp
 fi
-ln -s .bashrc $HOME/
+ln -s $rd/.bashrc $HOME/
 
 if [ -f $HOME/.bash_aliases ]
 then
-		mv $HOME/.bash_aliases
+		mv $HOME/.bash_aliases $tmp
 fi
-ln -s .bash_aliases $HOME/
+ln -s $rd/.bash_aliases $HOME/
 
+if [ -f $HOME/.inputrc ]
+then
+		mv $HOME/.inputrc $tmp
+fi
+ln -s $rd/.inputrc $HOME/
+
+if [ -f $HOME/.profile ]
+then
+		mv $HOME/.profile $tmp
+fi
+ln -s $rd/.profile $HOME/
+
+if [ -f $HOME/.tmux.conf ]
+then
+		mv $HOME/.tmux.conf $tmp
+fi
+ln -s $rd/.tmux.conf $HOME/
+
+
+# .config
+
+conf=$HOME/.config
+
+if [ ! -e $conf ]
+then
+		mkdir $conf
+fi
+
+if [ -f $conf/katerc ]
+then
+		mv $conf/katerc $tmp
+fi
+
+if [ -f $conf/katevirc ]
+then
+		mv $conf/katevirc $tmp
+fi
+
+if [ -e $conf/qutebrowser ]
+then
+		rm -rf $conf/qutebrowser
+fi
+
+if [ -e $conf/tridactyl ]
+then
+		rm -rf $conf/tridactyl
+fi
+
+if [ -e $conf/zathura ]
+then
+		rm -rf $conf/zathura
+fi
+ln -s $rd/.config/* $conf/
+
+# mozilla
+if [ ! -e $HOME/.mozilla ]
+then
+		mkdir -p $HOME/.mozilla/firefox/current/
+fi
+ln -s $rd/.mozilla/firefox/current/* $HOME/.mozilla/firefox/current/
+
+#.ssh
+if [ ! -e $HOME/.ssh ]
+then
+		mkdir $HOME/.ssh
+fi
+ln -s $rd/.ssh/ssh_alias $HOME/.ssh/

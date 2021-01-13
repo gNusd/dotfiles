7aedcff3235222d9125d633c741364e1d932221d
gnus <gnus@mailbox.org>
2021-01-13 22:31:02 +0100

    changes in init.vim

---
+++ .config/zsh/exports.zsh
@@ -0,0 +1,5 @@
+export KEYTIMEOUT=1
+export EDITOR=nvim
+export VISUAL=nvim
+export RUST_SRC_PATH="$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
+export PATH="$HOME/bin:$HOME/.cargo/bin:/var/lib/snapd/snap/bin/:/$HOME/.local/bin:/$HOME/.local/bin/scripts/:$PATH"
